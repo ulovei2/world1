19905002b562d951a711e0366b7eeb3c67cee23c
ulovei2 <42592143+ulovei2@users.noreply.github.com>
2018-08-22 10:49:49 +0700

    Add files via upload

--- testdb.php
+++ testdb.php
@@ -1,6 +1,6 @@
 <?php
 require('./db/connect-db.php');  //เรียกใช้ไฟล์.ในfolder db ไฟล์
-$sql_office = "SELECT * from tbl_office where office_name like '%J%'";
+$sql_office = "SELECT * from tbl_office where office like '%J%'";
 $query_office = mysqli_query($conn,$sql_office);
 while($obj= mysqli_fetch_array($query_office))
 {echo $obj["office_name"]."และ".$obj["office"]."<br>";		
