8f24bc329664ac0be7fa17a097578fcd10d89613
ulovei2 <42592143+ulovei2@users.noreply.github.com>
2018-08-22 10:22:58 +0700

    Add files via upload

--- testdb.php
+++ testdb.php
@@ -1,10 +1,9 @@
 <?php
-require('./db/connect-db.php);//เรียกใช้ไฟล์.ในfolder db ไฟล์
+require('./db/connect-db.php');  //เรียกใช้ไฟล์.ในfolder db ไฟล์
 $sql_office = "SELECT * from tbl_office";
 $query_office = mysqli_query($conn,$sql_office);
-while ($obj= mysqli_fetch_array($query_office))
-{echo $obj["office_name"]."<br>";	
-	
+while($obj= mysqli_fetch_array($query_office))
+{echo $obj["office_name"]."<br>";		
 	
 }
 
